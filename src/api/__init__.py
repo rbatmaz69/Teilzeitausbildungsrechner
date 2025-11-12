06eafe6ba48981dc0595cacaec3ded613120da10
Johannes Keller <jkeller5@stud.hs-heilbronn.de>
2025-11-12 16:42:13 +0100

    Fix isort error

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,7 +1,6 @@
 """API-Service-Schicht für das Teilzeitrechner-Backend."""
 
-from .calculation_service import (BerechnungsAnfrage,
-                                  BerechnungsDienstAntwort,
+from .calculation_service import (BerechnungsAnfrage, BerechnungsDienstAntwort,
                                   verarbeite_berechnungsanfrage)
 
 __all__ = [
